12309113f03ccb68e01ab5f051e2ea0b97f21bf7
Nidhi <nidhi1382005@gmail.com>
2024-11-25 16:48:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleRecipeDetails(id) {
+    const details = document.getElementById(id);
+    if (details.style.display === "none") {
+        details.style.display = "block";
+    } else(details.style.process = "block");
+}
